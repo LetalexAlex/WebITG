6e4d1486071829a190c2b7f6d18559ad189d970b
LetalexAlex <letalexalexx@gmail.com>
2026-04-18 01:23:06 +0200

    voglio dormire

--- client/src/render.js
+++ client/src/render.js
@@ -1,5 +1,5 @@
 // description: This example demonstrates how to use a Container to group and manipulate multiple sprites
-import { Application, Assets, Container, Sprite } from 'pixi.js';
+import { Application, Assets, Container, Sprite, Text, TextStyle} from 'pixi.js';
 
 (async () => {
     // Create a new application
@@ -8,38 +8,64 @@ import { Application, Assets, Container, Sprite } from 'pixi.js';
     // Initialize the application
     await app.init({ background: '#1099bb', resizeTo: window });
 
+    await Assets.load({
+        src: '../static/fonts/Fredoka-Regular.ttf',
+        data: {
+            family: 'Fredoka',
+        }
+    });
+    await Assets.load({
+        src: '../static/fonts/wendy.ttf',
+        data: {
+            family: 'Wendy',
+        }
+    });
+
     // Append the application canvas to the document body
     document.body.appendChild(app.canvas);
 
-    // Create and add a container to the stage
-    const container = new Container();
+    const myText = new Text({
+        text: '67',
+        style: {
+            fill: '#ffffff',
+            fontSize: 30,
+            fontFamily: 'Fredoka',
+        },
+    });
+    app.stage.addChild(myText);
 
-    app.stage.addChild(container);
 
-    // Load the bunny texture
-    const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+    const arrowContainer = new Container();
 
-    // Create a 5x5 grid of bunnies in the container
-    for (let i = 0; i < 25; i++) {
-        const bunny = new Sprite(texture);
+    app.stage.addChild(arrowContainer);
 
-        bunny.x = (i % 5) * 40;
-        bunny.y = Math.floor(i / 5) * 40;
-        container.addChild(bunny);
-    }
+    const arrowTexture = await Assets.load('../static/noteskins/Devcel/arrow.png');
 
-    // Move the container to the center
-    container.x = app.screen.width / 2;
-    container.y = app.screen.height / 2;
+    const arrowLeft = new Sprite({texture: arrowTexture, anchor: 0.5, x: 0});
+    arrowLeft.rotation = Math.PI / 2
+    arrowContainer.addChild(arrowLeft);
 
-    // Center the bunny sprites in local container coordinates
-    container.pivot.x = container.width / 2;
-    container.pivot.y = container.height / 2;
+    const arrowDown = new Sprite({texture: arrowTexture, anchor: 0.5, x: 64});
+    arrowDown.rotation = 0
+    arrowContainer.addChild(arrowDown);
 
-    // Listen for animate update
+    const arrowUp = new Sprite({texture: arrowTexture, anchor: 0.5, x: 128});
+    arrowUp.rotation = Math.PI
+    arrowContainer.addChild(arrowUp);
+
+    const arrowRight = new Sprite({texture: arrowTexture, anchor: 0.5, x: 192});
+    arrowRight.rotation = Math.PI / 2 * 3
+    arrowContainer.addChild(arrowRight);
+
+    arrowContainer.x = app.screen.width / 5 * 3.5;
+    arrowContainer.y = app.screen.height / 5 * 4;
+
     app.ticker.add((time) => {
-        // Continuously rotate the container!
-        // * use delta to create frame-independent transform *
-        container.rotation -= 0.01 * time.deltaTime;
+
     });
 })();
+
+
+function renderSteps(stepsText) {
+
+}
